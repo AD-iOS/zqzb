c988c97911cc6a74a23373bb80abbe4bd27bff4c
iOS-GM <1107154510@qq.com>
2025-07-25 15:49:55 +0800

    Auto-update 2025-07-25 15:49

--- update.sh
+++ update.sh
@@ -8,22 +8,22 @@ xz -c9 Packages > Packages.xz
 # 计算校验值
 md5_pkg=$(md5sum Packages | cut -d' ' -f1)
 size_pkg=$(wc -c < Packages)
-md5_gz=$(md5sum Packages.gz | cut -d' ' -f1)
-size_gz=$(wc -c < Packages.gz)
+md5_xz=$(md5sum Packages.xz | cut -d' ' -f1)
+size_xz=$(wc -c < Packages.xz)
 
-# 更新Release文件（修正后的awk命令）
+# 更新Release文件
 awk -v md5_pkg="$md5_pkg" \
     -v size_pkg="$size_pkg" \
-    -v md5_gz="$md5_gz" \
-    -v size_gz="$size_gz" '
+    -v md5_xz="$md5_xz" \
+    -v size_xz="$size_xz" '
 BEGIN {print_flag=0}
 /MD5Sum:/ {print; print_flag=1; next}
 print_flag==1 && /Packages$/ { 
     print " " md5_pkg " " size_pkg " Packages"
     next
 }
-print_flag==1 && /Packages.gz$/ { 
-    print " " md5_gz " " size_gz " Packages.gz"
+print_flag==1 && /Packages.xz$/ { 
+    print " " md5_xz " " size_xz " Packages.xz"
     print_flag=0
     next
 }
