508d795ca2daf9c94df839d8cff429fd67741c7b
iOS-GM <1107154510@qq.com>
2025-08-11 14:55:14 +0800

    Auto-update 2025-08-11 14:55

--- update.sh
+++ update.sh
@@ -6,12 +6,6 @@ cd /var/mobile/zqzb
 dpkg-scanpackages -m . /dev/null > Packages
 gzip -c Packages > Packages.gz
 
-# 计算校验值
-md5_pkg=$(md5sum Packages | cut -d' ' -f1)
-size_pkg=$(wc -c < Packages)
-md5_gz=$(md5sum Packages.gz | cut -d' ' -f1)
-size_gz=$(wc -c < Packages.gz)
-
 # 提交更新
 git add .
 git commit -m "Auto-update $(date +'%Y-%m-%d %H:%M')"
